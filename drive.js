4a4e532d58eb0658fd62fc237bfca91980f8086a
lafcha <charlotte.bronsard@gmail.com>
2022-04-21 15:43:54 +0200

    opening a file

--- drive.js
+++ drive.js
@@ -1,16 +1,30 @@
 
 import fs from 'fs/promises';
 
-export function getFilesFromDir() {
-    return fs.readdir('./data', { withFileTypes: true })
-  }
-  
+export function getFilesFromDir(path) {
+    return fs.readdir(path, { withFileTypes: true })
+}
+
 export function filesToAlpfiles(dirents) {
-  
+
     return dirents.map(dirent => {
         return {
             name: dirent.name,
             isFolder: dirent.isDirectory()
         }
     })
-  }
+}
+
+export function displayFile(path){
+    return fs.readFile(path, (err, data) => {
+      return data 
+      });
+
+}
+
+export function checkFileOrDirectory (path){
+
+    return fs.stat(path, (err, stats)=> {
+        return stats.isDirectory()
+      })
+}
--- server.js
+++ server.js
@@ -1,26 +1,49 @@
 
 import express from 'express';
-
+import fs from 'fs/promises';
 import * as drive from './drive.js'
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.json());
-app.use('/', express.static('frontend'))
+app.use('/', express.static('frontend'));
 
 app.get('/api/drive', (req, res) => {
 
-  drive.getFilesFromDir()
-      .then(files => drive.filesToAlpfiles(files))
-      .then(alpfiles => res.send(alpfiles))
-      .catch(error => {
-          res.sendStatus(400, "Le dossier n'existe pas")
-      })
-   
+  const path = './data';
+  drive.getFilesFromDir(path)
+    .then(files => drive.filesToAlpfiles(files))
+    .then(alpfiles => res.send(alpfiles))
+    .catch(error => {
+      res.sendStatus(400, "Le dossier n'existe pas")
+    })
 
-})
+});
+
+app.get('/api/drive/:name', (req, res) => {
+
+
+  const name = req.params.name;
+  const path = './data/'+ name;
+
+drive.checkFileOrDirectory(path)
+  .then (display => console.log(display))
+  .then(responseFileOrDirectory => {
+    if (responseFileOrDirectory) {
+      drive.getFilesFromDir(path)
+        .then(files => drive.filesToAlpfiles(files))
+        .then(alpfiles => res.send(alpfiles))
+    } else {
+      drive.displayFile(path)
+      .then(file => res.send(file))
+    } 
+  })
+  .catch(error => {
+    res.sendStatus(404, "Le dossier ou le fichier n'existe pas")
+  })
+});
 
 
 function start() {
@@ -28,7 +51,7 @@ function start() {
     console.log(`Example app listening on port ${port}`)
   })
 
-}
+};
 
 
 export { start }
